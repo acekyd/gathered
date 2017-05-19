df2936a774b60bdc7feb2a075d7975c3b3d8102f
Abati Adewale <acekyd01@gmail.com>
2017-05-19 08:46:43 +0100

    home dashboard

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { NavController, NavParams, Platform } from 'ionic-angular';
 
 import { MeetupService } from '../../providers/meetup-service';
+import { EventPage} from '../event/event';
 
 @Component({
   selector: 'page-home',
@@ -13,13 +14,23 @@ export class HomePage {
   public access_token = "";
   public meetup: any;
   public dashboard: any;
+  public user: any;
 
   constructor(public navCtrl: NavController,  public navParams: NavParams, public meetupService: MeetupService ) {
   		this.access_token = localStorage.getItem("access_token");
 	  	  if(this.access_token)
 	  	  {
 	  	    this.loadDashboard();
+          this.loadUser();
 	  	  }
+  }
+
+  public openEvent(urlname, id)
+  {
+    this.navCtrl.push(EventPage, {
+      urlname: urlname,
+      id: id
+    });
   }
 
   loadDashboard(){
@@ -28,11 +39,12 @@ export class HomePage {
       this.dashboard = data;
     });
   }
-  //Use this later
-  loadPeople(){
-    this.meetupService.load()
+
+  loadUser(){
+    this.meetupService.loadUser()
     .then(data => {
-      this.meetup = data;
+      this.user = data;
+      localStorage.setItem('user', JSON.stringify(this.user));
     });
   }
 
