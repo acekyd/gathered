f296efcd0b6b9b154f0e6876809f78ed0da20eb8
Abati Adewale <acekyd01@gmail.com>
2017-05-27 18:06:51 +0100

    Implemented search on connect

--- src/pages/contact/contact.ts
+++ src/pages/contact/contact.ts
@@ -9,12 +9,54 @@ import {AngularFireDatabase, FirebaseListObservable} from 'angularfire2/database
 export class ContactPage {
 
   connects: FirebaseListObservable<any>;
+  public connectsList: Array<any>;
   public user: any;
 
   constructor(public navCtrl: NavController, public alertCtrl: AlertController, public af: AngularFireDatabase, public actionSheetCtrl: ActionSheetController, public toastCtrl: ToastController) {
 	 this.user = JSON.parse(localStorage.getItem("user"));
 	 this.connects = af.list('/connects/'+this.user.id);
 	 console.log(this.connects);
+	 this.initializeItems();
+  }
+
+  initializeItems(){
+  	this.connectsList = [];
+	 console.log(this.connects);
+	 var self = this;
+	  	self.connects.forEach(function(element) {
+	        element.forEach(function(elem){
+	           self.connectsList.push(elem);
+	           console.log(self.connectsList);
+	          })
+	    })
+	console.log(this.connectsList);
+	console.log("bimpe");
+  }
+
+  getItems(searchbar) {
+  	console.log("you are mad");
+    // Reset items back to all of the items
+    this.initializeItems();
+
+    // set q to the value of the searchbar
+    var q = searchbar.srcElement.value;
+
+    // if the value is an empty string don't filter the items
+    if (!q) {
+      return;
+    }
+
+    this.connectsList = this.connectsList.filter((v) => {
+      if(v.name && q) {
+        if (v.name.toLowerCase().indexOf(q.toLowerCase()) > -1 || v.eventName.toLowerCase().indexOf(q.toLowerCase()) > -1 || v.bio.toLowerCase().indexOf(q.toLowerCase()) > -1) {
+          return true;
+        }
+        return false;
+      }
+    });
+
+    console.log(q, this.connectsList.length);
+
   }
 
 	 showOptions(connectId, connectName) {
